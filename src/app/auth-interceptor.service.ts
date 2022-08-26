da9534f8a40385e500d1dacd6e0f5b47cba5015f
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-26 13:51:49 -0400

    added multiple interceptors - auth + logging service

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,22 +4,14 @@ export class AuthInterceptorService implements HttpInterceptor {
 
     //! runs code before the request leaves application
     intercept(req: HttpRequest<any>, next: HttpHandler): any {
-        console.log('Request is on its way');
-        console.log(req.url)
+
 
         // ! modify request
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'xyz')
         });
 
-        return next.handle(modifiedRequest).pipe(tap(event => {
-            console.log(event)
-            if (event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data:')
-                console.log(event.body)
-            }
-
-        }))
+        return next.handle(modifiedRequest)
     }  //! request continues
 }
 
