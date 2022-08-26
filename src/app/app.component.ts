15436a364f3c7aa67ae8ab7e06b74f89b2cab158
Simon Cheam <scheamrevation.com@Simons-MacBook-Pro-2.local>
2022-08-25 21:13:51 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Post } from "./post.model";
+import { PostService } from "./posts.service";
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+
+  constructor(private http: HttpClient, private postsService: PostService) { }
+
+  ngOnInit() {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });;
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+
+    this.postsService.createAndStorePost(postData.title, postData.content)
+
+  }
+
+  onFetchPosts() {
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(posts => {
+      this.isFetching = false;
+      this.loadedPosts = posts;
+    });;
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.postsService.deletePosts().subscribe(() => {
+      this.loadedPosts = [];
+    })
+  }
+
+
+}
---
+++ src/app/posts.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Post } from "./post.model";
+import { map } from "rxjs/operators";
+
+
+@Injectable({ providedIn: 'root' })
+export class PostService {
+    loadedPosts: Post[] = [];
+
+
+    constructor(private http: HttpClient) { }
+
+
+    createAndStorePost(title: string, content: string) {
+
+        const postData: Post = { title: title, content: content }
+
+        //..
+        this.http
+            .post<{ name: string }>(
+                "https://test-project-34deb-default-rtdb.firebaseio.com/posts.json",
+                postData
+            )
+            .subscribe((responseData) => {
+                console.log(responseData);
+            });
+    }
+
+    fetchPosts() {
+        return this.http
+            .get<{ [key: string]: Post }>(
+                "https://test-project-34deb-default-rtdb.firebaseio.com/posts.json"
+            ) // ! added get response body Type
+            .pipe(
+                map((responseData) => {
+                    // ! wraps into observable
+                    const postsArray: Post[] = [];
+                    for (const key in responseData) {
+                        if (responseData.hasOwnProperty(key)) {
+                            postsArray.push({ ...responseData[key], id: key }); // ...pulls out key value pairs
+                        }
+                    }
+                    console.log(postsArray);
+                    return postsArray; // returned to subscribed function
+                })
+            )
+
+        // ..
+    }
+
+    deletePosts() {
+        return this.http.delete("https://test-project-34deb-default-rtdb.firebaseio.com/posts.json")
+    }
+
+}
